4e71747a5a8100e7ef5f05f90bf2f9e1631062c3
Nikolay Bukharin <nekronone@gmail.com>
2020-02-04 21:12:25 +0400

    Edits made

--- src/index.js
+++ src/index.js
@@ -17,11 +17,11 @@
    isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false
  */
 function isAllTrue(array, fn) {
-    if (!Array.isArray(array) || array.length == 0) {
-        throw new Error('empty array');
+    if (array instanceof Array == false || array.length == 0) {
+        throw new Error("empty array");
     }
-    if (typeof fn != 'function') {
-        throw new Error('fn is not a function');
+    if (typeof fn != "function") {
+        throw new Error("fn is not a function");
     }
     let times = 0;
 
@@ -51,17 +51,17 @@ function isAllTrue(array, fn) {
    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
  */
 function isSomeTrue(array, fn) {
-    if (!Array.isArray(array) || array.length == 0) {
-        throw new Error('empty array');
+    if (array instanceof Array == false || array.length == 0) {
+        throw new Error("empty array");
     }
-    if (typeof fn != 'function') {
-        throw new Error('fn is not a function');
+    if (typeof fn != "function") {
+        throw new Error("fn is not a function");
     }
     for (let i = 0; i < array.length; i++) {
         if (fn(array[i])) {
             return true;
         }
-    }  
+    }
 
     return false;
 }
@@ -78,8 +78,8 @@ function isSomeTrue(array, fn) {
    - fn не является функцией (с текстом "fn is not a function")
  */
 function returnBadArguments(fn, ...args) {
-    if (typeof fn != 'function') {
-        throw new Error('fn is not a function');
+    if (typeof fn != "function") {
+        throw new Error("fn is not a function");
     }
     const arr = [];
 
@@ -113,50 +113,46 @@ function returnBadArguments(fn, ...args) {
  */
 function calculator(number = 0) {
     if (isNaN(number)) {
-        throw new Error('number is not a number');
+        throw new Error("number is not a number");
     }
+
+    let result = number;
 
     return {
-        number: number,
-        sum: function(...args) {
+        sum: function (...args) {
             for (let i = 0; i < args.length; i++) {
-                this.number += args[i];
+                result += args[i];
+            }
+            
+            return result;
+        },
+        dif: function (...args) {
+            for (let i = 0; i < args.length; i++) {
+                result -= args[i];
             }
 
-            return this.number;
+            return result;
         },
-        dif: function(...args) {
-            for (let i = 0; i < args.length; i++) {
-                this.number -= args[i];
-            }
-
-            return this.number;
-        },
-        div: function(...args) {
+        div: function (...args) {
             for (let i = 0; i < args.length; i++) {
                 if (args[i] === 0) {
-                    throw new Error('division by 0');
+                    throw new Error("division by 0");
                 }
-                this.number /= args[i];
+                result /= args[i];
             }
 
-            return this.number;
+            return result;
         },
-        mul: function(...args) {
+        mul: function (...args) {
             for (let i = 0; i < args.length; i++) {
-                this.number *= args[i];
+                result *= args[i];
             }
 
-            return this.number;
+            return result;
         }
-    }
+    };
 }
 
 /* При решении задач, пострайтесь использовать отладчик */
 
-export {
-    isAllTrue,
-    isSomeTrue,
-    returnBadArguments,
-    calculator
-};
+export { isAllTrue, isSomeTrue, returnBadArguments, calculator };
